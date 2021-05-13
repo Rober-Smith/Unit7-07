f099a7cd9f8fb8dd5da1ef11b5493ad52b1a68c0
RoberSmith <>
2021-05-13 00:09:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let numberone = 0
+let numbertwo = 0
+let counter = 0
+let answer = 0
+
+document.getElementById('button').addEventListener('click', calculate)
+
+function calculate () {
+  numberone = document.getElementById('one').value
+  numberone = parseInt(numberone)
+  numbertwo = document.getElementById('two').value
+  numbertwo = parseInt(numbertwo)
+
+  for (counter = 0; counter < numberone; counter++)
+  (
+    answer = answer + numbertwo
+  )
+  document.getElementById('answer').innerHTML = answer
+}
